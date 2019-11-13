69ac85d8f40d49d7dde5908325d0bdb557b692f6
adigladi <adigladi@hotmail.com>
2019-11-13 01:17:41 +0100

    css edits

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,106 +1,120 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link } from 'gatsby'
-import { StaticQuery, graphql } from "gatsby"
-import { HelmetDatoCms } from 'gatsby-source-datocms'
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "gatsby";
+import { StaticQuery, graphql } from "gatsby";
+import { HelmetDatoCms } from "gatsby-source-datocms";
 
-import '../styles/index.sass'
+import "../styles/index.sass";
 
 const TemplateWrapper = ({ children }) => (
-  <StaticQuery query={graphql`
-    query LayoutQuery
-    {
-      datoCmsSite {
-        globalSeo {
-          siteName
-        }
-        faviconMetaTags {
-          ...GatsbyDatoCmsFaviconMetaTags
-        }
-      }
-      datoCmsHome {
-        seoMetaTags {
-          ...GatsbyDatoCmsSeoMetaTags
-        }
-        introTextNode {
-          childMarkdownRemark {
-            html
+  <StaticQuery
+    query={graphql`
+      query LayoutQuery {
+        datoCmsSite {
+          globalSeo {
+            siteName
+          }
+          faviconMetaTags {
+            ...GatsbyDatoCmsFaviconMetaTags
           }
         }
-        copyright
-      }
-      allDatoCmsSocialProfile(sort: { fields: [position], order: ASC }) {
-        edges {
-          node {
-            profileType
-            url
+        datoCmsHome {
+          seoMetaTags {
+            ...GatsbyDatoCmsSeoMetaTags
+          }
+          introTextNode {
+            childMarkdownRemark {
+              html
+            }
+          }
+          copyright
+        }
+        allDatoCmsSocialProfile(sort: { fields: [position], order: ASC }) {
+          edges {
+            node {
+              profileType
+              url
+            }
           }
         }
       }
-    }
-  `}
-  render={data => (
-    <div className="container">
-      <HelmetDatoCms
-        favicon={data.datoCmsSite.faviconMetaTags}
-        seo={data.datoCmsHome.seoMetaTags}
-      />
-      <div className="container__sidebar">
-        <div className="sidebar">
-          <h6 className="sidebar__title">
-            <Link to="/">Adrian Berger | Portfolio</Link>
-          </h6>
-          <div
-            className="sidebar__intro"
-            dangerouslySetInnerHTML={{
-              __html: data.datoCmsHome.introTextNode.childMarkdownRemark.html,
-            }}
-          />
-          <ul className="sidebar__menu">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-          </ul>
-          <p className="sidebar__social">
-            {data.allDatoCmsSocialProfile.edges.map(({ node: profile }) => (
-              <a
-                key={profile.profileType}
-                href={profile.url}
-                target="blank"
-                className={`social social--${profile.profileType.toLowerCase()}`}
-              > </a>
-            ))}
-          </p>
-          <div className="sidebar__copyright">{data.datoCmsHome.copyright}</div>
-        </div>
-      </div>
-      <div className="container__body">
-        <div className="container__mobile-header">
-          <div className="mobile-header">
-          <div className="mobile-header__logo">
-              <Link to="#">Adrian Berger | Portfolio</Link>
+    `}
+    render={data => (
+      <div className="container">
+        <HelmetDatoCms
+          favicon={data.datoCmsSite.faviconMetaTags}
+          seo={data.datoCmsHome.seoMetaTags}
+        />
+        <div className="container__sidebar">
+          <div className="sidebar">
+            <h6 className="sidebar__title">
+              <Link to="/">Adrian Berger | Portfolio</Link>
+            </h6>
+            <img
+              className="profileImg"
+              src="https://www.datocms-assets.com/17710/1573599660-bwsquare.jpeg"
+              alt=""
+            />
+            <div
+              className="sidebar__intro"
+              dangerouslySetInnerHTML={{
+                __html: data.datoCmsHome.introTextNode.childMarkdownRemark.html
+              }}
+            />
+            <ul className="sidebar__menu">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+            </ul>
+            <p className="sidebar__social">
+              {data.allDatoCmsSocialProfile.edges.map(({ node: profile }) => (
+                <a
+                  key={profile.profileType}
+                  href={profile.url}
+                  target="blank"
+                  className={`social social--${profile.profileType.toLowerCase()}`}
+                >
+                  {" "}
+                </a>
+              ))}
+            </p>
+            <div className="sidebar__copyright">
+              {data.datoCmsHome.copyright}
             </div>
-            
-              <div
-            className="sidebar__intro"
-            style={{textAlign: "end"}}
-            dangerouslySetInnerHTML={{
-              __html: data.datoCmsHome.introTextNode.childMarkdownRemark.html,
-            }}
-          />
-        
-            
           </div>
         </div>
-        {children}
+        <div className="container__body">
+          <div className="container__mobile-header">
+            <div className="mobile-header">
+            <img
+                className="profileImgSmall"
+                src="https://www.datocms-assets.com/17710/1573599660-bwsquare.jpeg"
+                alt=""
+              />
+              <div className="mobile-header__title">
+                <Link to="#">Adrian Berger | Portfolio</Link>
+              </div>
+
+              
+
+              <div
+                className="sidebar__intro mobile-header__logo"
+                dangerouslySetInnerHTML={{
+                  __html:
+                    data.datoCmsHome.introTextNode.childMarkdownRemark.html
+                }}
+              />
+            </div>
+          </div>
+          {children}
+        </div>
       </div>
-    </div>
     )}
   />
-)
+);
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.object,
-}
+  children: PropTypes.object
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;
